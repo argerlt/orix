7353caf1f6a55cf6deb0177a43938489f8c0d315
Håkon Wiik Ånes <hwaanes@gmail.com>
2026-06-05 20:42:47 +0200

    Improve type hints and handling of IPF color key properties
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- orix/plot/orientation_color_keys/ipf_color_key_tsl.py
+++ orix/plot/orientation_color_keys/ipf_color_key_tsl.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2018-2025 the orix developers
+# Copyright 2018-2026 the orix developers
 #
 # This file is part of orix.
 #
@@ -17,28 +17,27 @@
 # along with orix. If not, see <http://www.gnu.org/licenses/>.
 #
 
-import matplotlib.figure as mfigure
+from typing import TYPE_CHECKING, Literal, overload
+
 import numpy as np
 
 from orix.plot.direction_color_keys.direction_color_key_tsl import (
     DirectionColorKeyTSL,
 )
-from orix.plot.orientation_color_keys.ipf_color_key import IPFColorKey
+from orix.plot.orientation_color_keys.ipf_color_key import (
+    VALID_SAMPLE_DIRECTION,
+    IPFColorKey,
+)
 from orix.quaternion.orientation import Orientation
 from orix.quaternion.symmetry import Symmetry
-from orix.vector import Miller, Vector3d
+from orix.vector.miller import Miller
+
+if TYPE_CHECKING:  # pragma: no cover
+    import matplotlib.figure as mfigure
 
 
 class IPFColorKeyTSL(IPFColorKey):
-    """Assigns colors to orientations in a symmetry-aware manner.
-
-    IPF color keys work by defining symmetry-specific color maps
-    that convert every orientation into a color. Images such as EBSD
-    maps can then be colored based on the crystal vector aligned with
-    a queried sample direction. For the simple case where the sample
-    direction is aligned with the viewing direction, this is akin to
-    asking "what color is the crystal vector pointing out of the screen
-    at me."
+    r"""Assigns colors to orientations in a symmetry-aware manner.
 
     Parameters
     ----------
@@ -52,41 +51,38 @@ class IPFColorKeyTSL(IPFColorKey):
 
     Notes
     -----
-    The TSL colormaps map only the 11 Laue classes, as they can all
-    be described as weighted combinations of red, green, and blue. For
+    IPF color keys work by defining symmetry-specific color maps that
+    convert every orientation into a color. Images such as EBSD maps can
+    then be colored based on the crystal vector aligned with a queried
+    sample direction. For the simple case where the sample direction is
+    aligned with the viewing direction, this is akin to asking "what
+    color is the crystal vector pointing out of the screen at me."
+
+    The TSL colormaps map only the 11 Laue classes, as they can all be
+    described as weighted combinations of red, green, and blue. For
     details on this map as well as options for less limiting alternative
-    mappings, refer to "Orientations, Perfectly Colored"
-    https://doi.org/10.1107/S1600576716012942
+    mappings, refer to :cite:`nolze2016orientation`.
     """
 
     def __init__(
-        self,
-        symmetry: Symmetry,
-        direction: Vector3d | list | tuple | None = None,
+        self, symmetry: Symmetry, direction: VALID_SAMPLE_DIRECTION | None = None
     ) -> None:
-        if direction is not None:
-            if isinstance(direction, Miller):
-                raise ValueError(
-                    "The sample direction must be a sample vector, not a crystal vector"
-                )
-            if not isinstance(direction, Vector3d):
-                try:
-                    direction = Vector3d(np.asanyarray(direction))
-                except:
-                    raise ValueError("'direction' cannot be interpreted as a Vector3d")
-            if direction.size != 1:
-                raise ValueError("Only one sample direction can be given")
-        super().__init__(symmetry.laue, direction=direction)
+        # Symmetry converted to Laue by calling overwritten parsing
+        # method
+        super().__init__(symmetry=symmetry, direction=direction)
+
+    # -------------------------- Properties -------------------------- #
 
     @property
     def direction_color_key(self) -> DirectionColorKeyTSL:
+        """Return the direction color key of the IPF color key."""
         return DirectionColorKeyTSL(self.symmetry)
+
+    # ------------------------ Public methods ------------------------ #
 
     def orientation2color(self, orientation: Orientation) -> np.ndarray:
         """Return an RGB color per orientation given a Laue symmetry
         and a sample direction.
-
-        Plot the inverse pole figure color key with :meth:`plot`.
 
         Parameters
         ----------
@@ -96,25 +92,46 @@ class IPFColorKeyTSL(IPFColorKey):
         Returns
         -------
         rgb
-            Color array of shape ``orientation.shape + (3,)``.
+            Color array of *orientation* shape + (3,).
+
+        See Also
+        --------
+        :meth:`plot`
         """
-        # TODO: Take crystal axes into account, by using Miller instead
-        # of Vector3d
-        m = orientation * self.direction
-        rgb = self.direction_color_key.direction2color(m)
+        # v_crystal = g_s2c * v_sample
+        v_crys: Miller = orientation * self.direction
+
+        rgb = self.direction_color_key.direction2color(v_crys)
+
         return rgb
 
-    def plot(self, return_figure: bool = False) -> mfigure.Figure | None:
+    @overload
+    def plot(
+        self, return_figure: Literal[False] = False
+    ) -> None: ...  # pragma: no cover
+
+    @overload
+    def plot(
+        self, return_figure: Literal[True] = True
+    ) -> "mfigure.Figure": ...  # pragma: no cover
+
+    def plot(self, return_figure: bool = False) -> "mfigure.Figure | None":
         """Plot the inverse pole figure color key.
 
         Parameters
         ----------
         return_figure
-            Whether to return the figure. Default is ``False``.
+            Whether to return the figure. Default is False.
 
         Returns
         -------
         figure
-            Color key figure, returned if ``return_figure=True``.
+            Color key figure, returned if *return_figure* is True.
         """
         return self.direction_color_key.plot(return_figure=return_figure)
+
+    # ----------------------- Private methods ------------------------ #
+
+    def _parse_symmetry_or_raise(self, symmetry: Symmetry) -> Symmetry:
+        symmetry = super()._parse_symmetry_or_raise(symmetry)
+        return symmetry.laue
