a194e8b13d9f240ece02c0edd52e5c596d9eb23a
Austin Gerlt <argerlt@gmail.com>
2026-07-08 13:03:42 -0600

    Allow weighted quaternion averages and update docstring

--- orix/quaternion/quaternion.py
+++ orix/quaternion/quaternion.py
@@ -1114,8 +1114,15 @@ class Quaternion(Object3d):
         dots = np.tensordot(self.data, other.data, axes=(-1, -1))
         return dots
 
-    def mean(self) -> Quaternion:
-        """Return the mean quaternion with unitary weights.
+    def mean(self, weights: np.ndarray | None = None) -> Quaternion:
+        r"""Return the mean quaternion.
+
+        Parameters
+        ----------
+        weights
+            An optional array of weights for calculating a weighted
+            average instead of the unweighted mean. Must be the same
+            size as the quaternion array.
 
         Returns
         -------
@@ -1124,14 +1131,41 @@ class Quaternion(Object3d):
 
         Notes
         -----
-        The method used here corresponds to Equation (13) in
-        https://arc.aiaa.org/doi/pdf/10.2514/1.28949.
+        The method used here corresponds to equations 12 and 13 in
+        :cite:`markley_averaging_2007`, which simplifies to the
+        following two equations:
+
+            .. math::
+
+                M = \sum_{n=1}^{n}{wq_i \cdot q_i^T}
+
+                q_{mean} = \max_{q \in SO(3)} \left( q^T \cdot M \cdot q \right)
+
+        Which gives the Frobenius norm of rotation space (SO(3)). This
+        is different from the following Euclidean-style equation
+        occasionally used elsewhere in crystallography:
+
+            .. math::
+
+                q_{mean} = norm\left( \frac{\sum_{n=1}^{n}{wq_i }}{n} \right)
+
+        Which gives the Frobenius norm of quaternion space (SU(2)).
+        These are roughly identical for closely clustered quaternions,
+        but as explained in :cite:`markley_averaging_2007`,
+        increasingly deviate as the spread in the data increases.
         """
         Q = self.flatten().data.T
-        QQ = Q.dot(Q.T)
+        if weights is not None:
+            weights = np.asanyarray(weights).flatten()[:, np.newaxis]
+            QQ = Q.dot(weights * Q.T)
+        else:
+            QQ = Q.dot(Q.T)
         w, v = np.linalg.eig(QQ)
-        w_max = np.argmax(w)
-        return self.__class__(v[:, w_max])
+        v_mean = v[:, np.argmax(w)]
+        # flip if necessary.
+        if v_mean[0] < 0:
+            v_mean = v_mean * -1
+        return self.__class__(v_mean)
 
     def outer(
         self,
